189e55f2fe438ddfbf48cccae17e4c93d24e5c37
incetro <incetro@ya.ru>
2017-07-10 11:15:30 +0300

    Added sources

---
+++ Message/Message.h
@@ -0,0 +1,19 @@
+//
+//  Message.h
+//  Message
+//
+//  Created by incetro on 10/07/2017.
+//  Copyright © 2017 Incetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Message.
+FOUNDATION_EXPORT double MessageVersionNumber;
+
+//! Project version string for Message.
+FOUNDATION_EXPORT const unsigned char MessageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Message/PublicHeader.h>
+
+
